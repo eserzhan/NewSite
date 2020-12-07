dd6a76e2e97293977a5302a923aa8bdb43be2119
Yerasil <e_serzhan@mail.ru>
2020-12-07 14:24:26 +0600

    first commit

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\admin\desktop\django with bekzat\django girls\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
